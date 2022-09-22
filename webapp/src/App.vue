e422fbe87a7f5345c5910ae32412b2c617b37cfa
Jaime <jaimeloeuf@gmail.com>
2022-09-22 12:20:33 +0800

    Add support for async components using Suspense.

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -7,7 +7,35 @@ import Navbar from "./components/Navbar.vue";
 
   <!-- Center router view element horizontally -->
   <div class="container">
-    <router-view />
+    <!--
+      RouterView is a component that loads a component (usually dynamically) based on
+      the current URL route. However since there are many async SFCs defined, they
+      cannot be rendered directly with <RouterView />. Therefore `Suspense` is added
+      at the top level to await and ensure all is loaded before anything is shown.
+
+      RouterView component has a default slot, and the route Component is passed into
+      the default slot. Instead of passing it to `component` directly, the rendering
+      with `component` is nested in Suspense.
+
+      Reference links:
+      - https://router.vuejs.org/guide/advanced/lazy-loading.html
+      - https://vuejs.org/guide/components/async.html
+      - https://vuejs.org/guide/built-ins/suspense.html
+    -->
+    <RouterView v-slot="{ Component }">
+      <template v-if="Component">
+        <Suspense>
+          <!-- Show the Component selected by the router dynamically using 'component' -->
+          <component :is="Component"></component>
+
+          <!-- Loading component -->
+          <template #fallback>
+            <!-- @todo Create an actual full screen loader component -->
+            Loading...
+          </template>
+        </Suspense>
+      </template>
+    </RouterView>
   </div>
 </template>
 
