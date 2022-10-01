91931d2ca76a8dc5caa9d3c8356aef27969aa223
Jaime <jaimeloeuf@gmail.com>
2022-10-01 16:10:36 +0800

    Add test for block controller.

--- server-nest/src/blocks/blocks.controller.spec.ts
+++ server-nest/src/blocks/blocks.controller.spec.ts
@@ -1,20 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlocksController } from './blocks.controller';
 import { BlocksService } from './blocks.service';
+import { PrismaService } from '../providers/prisma.service';
+
+import type { block as Block } from '@prisma/client';
 
 describe('BlocksController', () => {
   let controller: BlocksController;
+  let service: BlocksService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [BlocksController],
-      providers: [BlocksService],
+      providers: [BlocksService, PrismaService],
     }).compile();
 
-    controller = module.get<BlocksController>(BlocksController);
+    service = moduleRef.get<BlocksService>(BlocksService);
+    controller = moduleRef.get<BlocksController>(BlocksController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should be able to save a new block', async () => {
+    const block: Block = {
+      id: 'SOP-cs9lri8508001ocjv9c44oxz5',
+      type: 'SOP',
+      parentID: null,
+      properties: {
+        title: 'My SOP',
+      },
+
+      // @todo To use string dates instead
+      // createdAt: '2022-10-01T04:25:57.520Z',
+      // updatedAt: '2022-10-01T04:25:57.522Z',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const createBlockDto = {
+      type: 'SOP',
+      properties: {
+        title: 'My SOP',
+      },
+    };
+
+    jest
+      .spyOn(service, 'create')
+      .mockImplementation(async (_createBlockDto) => block);
+
+    expect(await service.create(createBlockDto)).toBe(block);
   });
 });
