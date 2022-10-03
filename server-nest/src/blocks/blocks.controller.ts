d6be1c9d0e379a72fb40554c41af03bef968f01a
Jaime <jaimeloeuf@gmail.com>
2022-10-03 12:44:56 +0800

    Rename method and add comments in BlockController.

--- server-nest/src/blocks/blocks.controller.ts
+++ server-nest/src/blocks/blocks.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { BlocksService } from './blocks.service';
 import { CreateBlockDto } from './dto/create-block.dto';
+// import { UpdateBlockDto } from './dto/update-block.dto';
 
 @Controller('blocks')
 export class BlocksController {
@@ -12,9 +13,16 @@ export class BlocksController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  getBlocks(@Param('id') id: string) {
     return this.blocksService.getBlockAndChildren(id);
   }
+
+  // @todo Is an update API needed or just delete and create a new one?
+  // @todo Because without an update API, the updatedAt time stamp in the DB can be removed too
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateBlockDto: UpdateBlockDto) {
+  //   return this.blocksService.update(id, updateBlockDto);
+  // }
 
   @Delete(':id')
   deleteBlock(@Param('id') id: string) {
